51bff0aaa6ae573568911381ebcecf74e770cd11
Abdul <abdul@jaiecom.com>
2022-04-23 23:21:03 +0300

    api rate limit added, only 5 requests allowed in a minute

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import('./config/mongoose.js');
 import router from './routes/index.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import rateLimit from 'express-rate-limit';
+
 const __filename = fileURLToPath(import.meta.url); // get the file name
 const __dirname = path.dirname(__filename); // get directory name from file name
 const app = express();
@@ -31,7 +33,17 @@ mongoose.connect(MONGODB_URL, {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000000 }));
 
-   
+const apiRequestLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 5, // limit each IP to 5 requests per windowMs
+    handler: function (req, res, /*next*/) {
+        return res.status(429).json({
+          status: false,  
+          message: 'You sent too many requests. Please wait a while then try again'
+        })
+    }
+});
+app.use(apiRequestLimiter)
 // import routes index file 
 app.use('', router);
 const PORT = process.env.PORT || 3000;
